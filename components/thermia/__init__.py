4f4a238a85d49c3933345111ff12722f437ac9c5
Mattias Tornblad <mattias.tornblad@gmail.com>
2025-11-15 12:06:02 +0100

    changes

--- components/thermia/__init__.py
+++ components/thermia/__init__.py
@@ -14,20 +14,11 @@ DEPENDENCIES = ['i2c']
 
 thermia_ns = cg.esphome_ns.namespace('thermia')
 Thermia = thermia_ns.class_('Thermia', i2c.I2CDevice, cg.Component)
-CONF_ENCODER_FILTER = "encoder_filter"
-CONF_BUTTON = "button"
-CONF_ENCODER = "encoder"
-
-THERMIA_SCHEMA = cv.Schema(
-    {
-        cv.Optional(CONF_ENCODER_FILTER, default=1): cv.int_range(min=1, max=100),
-    }
-).extend(sensor.sensor_schema())
+CONF_THERMIA = "thermia"
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(Thermia),
-        cv.Optional(CONF_ENCODER): THERMIA_SCHEMA,
     }
 ).extend(i2c.i2c_device_schema(0x42))
 
@@ -36,11 +27,3 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
-    if CONF_ENCODER in config:
-        sens = await sensor.new_sensor(config[CONF_ENCODER])
-        cg.add(var.set_encoder(sens))
-
-        encoderConfig = config[CONF_ENCODER]
-
-        if CONF_ENCODER_FILTER in encoderConfig:
-            cg.add(var.set_encoder_filter(encoderConfig[CONF_ENCODER_FILTER]))
